07a2c9db99978919ba46ef4faf69de81e36bb74a
Erik Berndt <e.berndt@bbqberlin.onmicrosoft.com>
2023-01-31 15:44:07 +0100

    rebus neu

--- rebus/index.php
+++ rebus/index.php
@@ -1,40 +1,59 @@
-<!DOCTYPE html>
-
-<html lang="de">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <title></title>
-  </head>
-  <body>
-    <div class="rebus">
-      <div class="pic">
-        <img src="" alt="">
-      </div>
-      <div class="pic">
-        <img src="" alt="">
-      </div>
-      <div class="pic">
-        <img src="" alt="">
-      </div>
-    </div>
-    <div>
-      <form method="POST">
-          <p>Such dir die schönste Antwort aus</p>
-          <input class="radio" id="one" type="radio" name="answers" value="answer1">
-          <label for="one">Antwort 1</label><br>
-          <input class="radio" id="two" type="radio" name="answers" value="answer2">
-          <label for="two">Antwort 2</label><br>
-          <input class="radio" id="three" type="radio" name="answers" value="answer3">
-          <label for="three">Antwort 3</label><br>
-          <input class="radio" id="four" type="radio" name="answers" value="answer4">
-          <label for="three">Antwort 4</label><br><br>
-          <input class="submit" type="submit" value="oké">
-      </form>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="public/js/app.js"></script>
-  </body>
+ <!DOCTYPE html> 
+<html> 
+  <head> 
+    <title>Rebus</title> 
+</head> 
+<body> 
+<table border="1px solid black" align="center"> 
+  <caption> 
+    <h1>Bilder Quiz</h1> 
+  </caption> 
+  <tr> 
+    <td> 
+      <img src="bilder/bilder_raetsel_1.png" alt="Bilderrätsel 1" width="400" height="250"> 
+    </td> 
+  </tr> 
+  <tr>  
+    <td> 
+      <p>Such dir die schönste Antwort aus</p> 
+    </td> 
+  </tr> 
+  <tr> 
+    <form method="POST"> 
+      <tr>  
+        <td> 
+          <label> 
+            <input type="radio" name="antwort_gruppe" value="antwort_1">Antwort 1 
+          </label> 
+        </td> 
+      </tr> 
+      <tr>  
+        <td> 
+          <label> 
+            <input type="radio" name="antwort_gruppe" value="antwort_2">Antwort 2 
+          </label> 
+        </td> 
+      </tr> 
+      <tr>  
+        <td> 
+          <label> 
+            <input type="radio" name="antwort_gruppe" value="antwort_3">Antwort 3 
+          </label> 
+        </td> 
+      </tr> 
+      <tr>  
+        <td> 
+          <label> 
+            <input type="radio" name="antwort_gruppe" value="antwort_4">Antwort 4 
+          </label> 
+        </td> 
+      </tr> 
+      <tr>  
+        <td> 
+          <input type="submit" value="antworten"> 
+        </td> 
+      </tr> 
+    </form> 
+    </table> 
+  </body> 
 </html>
