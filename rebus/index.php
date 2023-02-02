1fb62acf5800fe5148a02e660a7f3bf30152a129
Erik Berndt <e.berndt@bbqberlin.onmicrosoft.com>
2023-02-02 14:26:40 +0100

    add pics, dynamic html injection

--- rebus/index.php
+++ rebus/index.php
@@ -1,59 +1,72 @@
- <!DOCTYPE html> 
-<html> 
-  <head> 
-    <title>Rebus</title> 
-</head> 
-<body> 
-<table border="1px solid black" align="center"> 
-  <caption> 
-    <h1>Bilder Quiz</h1> 
-  </caption> 
-  <tr> 
-    <td> 
-      <img src="bilder/bilder_raetsel_1.png" alt="Bilderrätsel 1" width="400" height="250"> 
-    </td> 
-  </tr> 
-  <tr>  
-    <td> 
-      <p>Such dir die schönste Antwort aus</p> 
-    </td> 
-  </tr> 
-  <tr> 
-    <form method="POST"> 
-      <tr>  
-        <td> 
-          <label> 
-            <input type="radio" name="antwort_gruppe" value="antwort_1">Antwort 1 
-          </label> 
-        </td> 
-      </tr> 
-      <tr>  
-        <td> 
-          <label> 
-            <input type="radio" name="antwort_gruppe" value="antwort_2">Antwort 2 
-          </label> 
-        </td> 
-      </tr> 
-      <tr>  
-        <td> 
-          <label> 
-            <input type="radio" name="antwort_gruppe" value="antwort_3">Antwort 3 
-          </label> 
-        </td> 
-      </tr> 
-      <tr>  
-        <td> 
-          <label> 
-            <input type="radio" name="antwort_gruppe" value="antwort_4">Antwort 4 
-          </label> 
-        </td> 
-      </tr> 
-      <tr>  
-        <td> 
-          <input type="submit" value="antworten"> 
-        </td> 
-      </tr> 
-    </form> 
-    </table> 
-  </body> 
-</html>
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+   <title>Rebus</title>
+   <link rel="stylesheet" href="public/css/style.css">
+ </head>
+
+ <body>
+   <div class="container">
+     <table>
+       <caption>
+         <h1>Bilder Quiz</h1>
+       </caption>
+       <tr>
+         <td>
+           <img src="public/images/quest03.png" alt="Bilderrätsel 1" width="150">
+           <img src="public/images/quest02.png" alt="Bilderrätsel 2" width="150">
+           <img src="public/images/quest05.png" alt="Bilderrätsel 3" width="150">
+         </td>
+       </tr>
+       <tr>
+         <td>
+           <p>Such dir die schönste Antwort aus:</p>
+           <hr>
+         </td>
+       </tr>
+       <tr>
+         <form method="POST">
+           <?php
+            $antworten = array(
+              ' Antwörd 1' => 'falsch',
+              ' Antwort 2' => 'richtig',
+              ' Antword 3' => 'falsch',
+              ' AntwÖrt 4' => 'falsch'
+            );
+
+            $name = 'antwort_gruppe';
+
+            foreach ($antworten as $key => $value) {
+              echo "<tr><td><label>
+                    <input type='radio' name='antwort_gruppe' value=$value>$key 
+                  </label> 
+          </td></tr>";
+            }
+            ?>
+       <tr>
+         <td>
+           <input type="submit" value="antworten">
+         </td>
+       </tr>
+       <tr>
+         <td>
+           <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              if (!array_key_exists('antwort_gruppe', $_POST)) {
+                echo "Du musst etwas auswählen!";
+              } else {
+                $inhalt = $_POST['antwort_gruppe'];
+                echo "<h1>$inhalt !!!!!</h1>";
+              }
+            }
+            ?>
+         </td>
+       </tr>
+       </form>
+
+     </table>
+   </div>
+ </body>
+
+ </html>
