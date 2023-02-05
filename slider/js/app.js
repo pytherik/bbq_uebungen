9146c494b01f5e511d7a5cb771c85ae9361039ac
pytherik <erikberndt@gmx.net>
2023-02-05 11:07:32 +0100

    format values

--- slider/js/app.js
+++ slider/js/app.js
@@ -1,5 +1,5 @@
 const progress = document.querySelector('.slider-value');
-const slider = document.querySelector('.slider-shape');
+const slider1 = document.querySelector('.slider-shape');
 const container = document.querySelector('.slider-container');
 const val_1 = document.querySelector('#val_1');
 
@@ -7,27 +7,34 @@ const styles = window.getComputedStyle(container);
 const maxWidth = styles.width.slice(0,-2);
 const valFactor = 255 / maxWidth;
 
+let sliderValue = 50;
+progress.style.width = `50px`;
+val_1.innerHTML = `00${sliderValue}`.slice(-3);
+
 let mouseDown = false;
 
-slider.addEventListener('click', (e) => {
+slider1.addEventListener('click', (e) => {
   e.preventDefault();
-  val_1.innerHTML = sliderValue;
+  sliderValue = Math.round(e.offsetX * valFactor);
+  val_1.innerHTML = `00${sliderValue}`.slice(-3);
   progress.style.width = `${e.offsetX}px`;
 })
 
-slider.addEventListener('mousedown', (e) => {
+
+
+slider1.addEventListener('mousedown', (e) => {
    mouseDown = true;
 })
 
-slider.addEventListener('mousemove', (e) => {
+slider1.addEventListener('mousemove', (e) => {
   e.preventDefault();
   if (mouseDown == true) {
-    const sliderValue = Math.round(e.offsetX * valFactor);
-    val_1.innerHTML = sliderValue;
+    sliderValue = Math.round(e.offsetX * valFactor);
+    val_1.innerHTML = `00${sliderValue}`.slice(-3);
     progress.style.width = `${e.offsetX}px`;
   }
 })
 
-slider.addEventListener('mouseup', () => {
+slider1.addEventListener('mouseup', () => {
   mouseDown = false;
 })
