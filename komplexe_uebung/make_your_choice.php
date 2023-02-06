fa6f3fa5167b103d717c6a9fa112e6d168c001df
Erik Berndt <e.berndt@bbqberlin.onmicrosoft.com>
2023-02-06 15:48:51 +0100

    add komplexe uebung

---
+++ komplexe_uebung/make_your_choice.php
@@ -0,0 +1,22 @@
+<div class="choices-container">
+  <form action="GET">
+    <div class="choice" id="choice1">
+      <div class="thumb">
+        <img src="public/images/quest01.png" alt="">
+      </div>
+      <input type="submit" value="raetsel1" id="btn1">
+    </div>  
+    <div class="choice" id="choice2">
+      <div class="thumb">
+        <img src="public/images/quest02.png" alt="">
+      </div>
+      <input type="submit" value="raetsel2" id="btn2">
+    </div>  
+    <div class="choice" id="choice3">
+      <div class="thumb">
+        <img src="public/images/quest03.png" alt="">
+      </div>
+      <input type="submit" value="raetsel3" id="btn3">
+    </div>  
+  </form>
+</div>
